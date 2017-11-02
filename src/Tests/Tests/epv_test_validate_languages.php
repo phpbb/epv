5474cd395396cbfe1b64df8acbad5335afeb61ab
kasimi <mail@kasimi.net>
2017-11-02 12:27:07 +0100

    Updated copyright

--- src/Tests/Tests/epv_test_validate_languages.php
+++ src/Tests/Tests/epv_test_validate_languages.php
@@ -1,10 +1,11 @@
 <?php
+
 /**
  *
  * EPV :: The phpBB Forum Extension Pre Validator.
  *
- * @copyright (c) 2014 phpBB Limited <https://www.phpbb.com>
- * @license       GNU General Public License, version 2 (GPL-2.0)
+ * @copyright (c) 2017 phpBB Limited <https://www.phpbb.com>
+ * @license GNU General Public License, version 2 (GPL-2.0)
  *
  */
 
