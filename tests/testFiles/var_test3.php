9301da63bcd64b0816363a02eac4eacd6f1c8018
Matt Friedman <maf675@gmail.com>
2025-05-10 17:31:02 -0700

    Update unit test

--- tests/testFiles/var_test3.php
+++ tests/testFiles/var_test3.php
@@ -8,6 +8,7 @@ class var_test3
     public function __construct()
     {
         $name = 'test';
-        $result = $this->{"validate_$name"}();
+        $result1 = $this->{"validate_$name"}();
+		$result2 = $this->{$name === 'test' ? 'callBack1' : 'callBack2'}();
     }
 }
