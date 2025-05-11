3a4f138e9420a457de7e31664c3935e5ccbe9cd5
Matt Friedman <maf675@gmail.com>
2025-05-11 08:58:53 -0700

    Simplify internal testing files

--- tests/testFiles/var_test2.php
+++ tests/testFiles/var_test2.php
@@ -8,6 +8,9 @@ class var_test2
     public function __construct()
     {
         $name = 'test';
-        $result = $this->{'validate_' . $name}();
-    }
+        $result1 = $this->{'validate1_' . $name}();
+		$result2 = $this->{"validate2_$name"}();
+		$result3 = $this->{$name === 'test' ? 'callBack1' : 'callBack2'}();
+
+	}
 }
